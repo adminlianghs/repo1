29b512a9ab444d077cf6ed152e3ab77c32089224
liang <liang669983@163.com>
2021-02-05 17:07:01 +0800

    更新信息

---
+++ HelloProjet/src/Test01.java
@@ -0,0 +1,9 @@
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+        System.out.println(1111);
+    }
+}
